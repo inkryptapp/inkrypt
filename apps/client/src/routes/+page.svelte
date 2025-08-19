52a67f8f0ff61eb271ca2cca11865daa44e5a44a
Peter Bartha <peterbartha@pm.me>
2025-08-19 23:35:11 +0200

    feat(app): create Tauri app with Svelte frontend

---
+++ apps/client/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { invoke } from '@tauri-apps/api/core';
+
+  let name = $state('');
+  let greetMsg = $state('');
+
+  async function greet(event: Event) {
+    event.preventDefault();
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsg = await invoke('greet', { name });
+  }
+</script>
+
+<main class="container">
+  <h1 class="text-2xl underline">Welcome to Tauri + Svelte</h1>
+
+  <form class="row" onsubmit={greet}>
+    <input placeholder="Enter a name..." bind:value={name} />
+    <button type="submit">Greet</button>
+  </form>
+  <p>{greetMsg}</p>
+</main>
