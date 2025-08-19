2d91e8f50aa5da9a3415e34adb18f3754ea59cde
Peter Bartha <peterbartha@pm.me>
2025-08-20 01:58:40 +0200

    chore: replace ESLint with Biome

--- packages/ui/src/utils.ts
+++ packages/ui/src/utils.ts
@@ -2,12 +2,12 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// biome-ignore lint/suspicious/noExplicitAny: third-party code
 export type WithoutChild<T> = T extends { child?: any } ? Omit<T, 'child'> : T;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// biome-ignore lint/suspicious/noExplicitAny: third-party code
 export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, 'children'> : T;
 export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
 export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
--- packages/ui/svelte.config.js
+++ packages/ui/svelte.config.js
@@ -3,14 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter(),
-		alias: {
-			'@inkrypt/ui': 'src',
-			'@inkrypt/ui/*': 'src/*'
-		}
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    alias: {
+      '@inkrypt/ui': 'src',
+      '@inkrypt/ui/*': 'src/*',
+    },
+  },
 };
 
 export default config;
