9d7b05b248a2953c60e3ee8d49f52f54233bcac6
Peter Bartha <peterbartha@pm.me>
2025-08-19 22:38:14 +0200

    chore: set up linting and formatting

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import { fileURLToPath } from 'node:url';
+import tseslint from 'typescript-eslint';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+export default defineConfig([
+  includeIgnoreFile(gitignorePath),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  tseslint.configs.recommended,
+]);
